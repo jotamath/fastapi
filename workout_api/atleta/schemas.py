fd8fe0d301f42ddd66d13f53974596c9dda657ec
jotamath <01jmnc@gmail.com>
2024-10-16 21:49:53 -0300

    bugfix: ajustar erros de db

--- workout_api/atleta/schemas.py
+++ workout_api/atleta/schemas.py
@@ -1,12 +1,23 @@
+from typing import Annotated, Optional
 from pydantic import Field, PositiveFloat
-from typing import Annotated
+#from workout_api.categorias.schemas import CategoriaIn
+#from workout_api.centro_treinamento.schemas import CentroTreinamentoAtleta
+
 from workout_api.contrib.schemas import BaseSchema
 
-class Atleta(BaseSchema):
+class AtletaIn(BaseSchema):
     nome: Annotated[str, Field(description='Nome do atleta', example='Joao', max_length=50)]
     cpf: Annotated[str, Field(description='CPF do atleta', example='12345678900', max_length=11)]
     idade: Annotated[int, Field(description='Idade do atleta', example=25)]
     peso: Annotated[PositiveFloat, Field(description='Peso do atleta', example=75.5)]
     altura: Annotated[PositiveFloat, Field(description='Altura do atleta', example=1.74)]
     sexo: Annotated[str, Field(description='Sexo do atleta', example='M', max_length=1)]
-    
+
+
+
+class AtletaOut(AtletaIn):
+    pass
+
+class AtletaUpdate(BaseSchema):
+    nome: Annotated[Optional[str], Field(None, description='Nome do atleta', example='Joao', max_length=50)]
+    idade: Annotated[Optional[int], Field(None, description='Idade do atleta', example=25)]
--- workout_api/contrib/schemas.py
+++ workout_api/contrib/schemas.py
@@ -1,8 +1,14 @@
-
-from pydantic import BaseModel
+from typing import Annotated
+from sqlalchemy import DateTime
+from pydantic import UUID4, BaseModel, Field
 
 
 class BaseSchema(BaseModel):
     class Config:
         extra = 'forbid'
         from_attributes= True
+
+#TODO
+class OutMixin(BaseModel):
+    id: Annotated(UUID4, Field(description='Identificador'))
+    create_at: DateTime
