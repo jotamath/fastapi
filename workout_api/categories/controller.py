3ab93e1cfd216a24e8cbe72814146ffa6dd49c78
jotamath <01jmnc@gmail.com>
2024-10-21 23:50:54 -0300

    bugfix make run

--- workout_api/categories/controller.py
+++ workout_api/categories/controller.py
@@ -1,16 +1,17 @@
 from fastapi import APIRouter, Body, status
-from workout_api.atleta.schemas import AtletaIn
+from workout_api.categories.schemas import CategoriaOut, CategoriaIn
 from workout_api.contrib.dependencies import DatabaseDependency
 
 router = APIRouter()
 
 @router.post(
     '/',
-    summary='Criar novo atleta',
-    status_code=status.HTTP_201_CREATED
+    summary='Criar nova categoria',
+    status_code=status.HTTP_201_CREATED,
+    response_model=CategoriaOut,
 )
 async def post(
     db_session: DatabaseDependency,
-    atleta_in: AtletaIn = Body(...)
+    atleta_in: CategoriaIn = Body(...)
 ):
     pass
--- workout_api/configs/database.py
+++ workout_api/configs/database.py
@@ -3,11 +3,15 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from workout_api.configs.settings import settings
 
-
+# Criação do engine assíncrono
 engine = create_async_engine(settings.DB_URL, echo=False)
+
+# Configuração do sessionmaker utilizando AsyncSession
 async_session = sessionmaker(
-    engine, class_=AsyncGenerator, expire_on_commit=False
+    engine, class_=AsyncSession, expire_on_commit=False
 )
-async def get_session() -> AsyncGenerator:
+
+# Função para obter a sessão de forma assíncrona
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session() as session:
         yield session
